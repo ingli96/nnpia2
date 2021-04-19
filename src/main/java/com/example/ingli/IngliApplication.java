db22162f1c6fdc4e460b56493891becdde8e264c
ingli96 <alexandr.anat.kireev@gmail.com>
2021-04-19 11:11:53 +0200

    itin

---
+++ src/main/java/com/example/ingli/IngliApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngliApplication.class, args);
+	}
+
+}
